dc3d0f2f663ed3725177ab14047a79d7c8e974e9
johannes003 <johannesandres03@gmail.com>
2024-08-05 14:12:47 -0500

    npm

--- app.js
+++ app.js
@@ -3,24 +3,24 @@ const path = require('path');
 const app = express();
 const port = 3010;
 
+// Configura el motor de plantillas EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Configura la carpeta estática 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas para archivos EJS
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/home.ejs'));
+    res.render('home');
 });
 
 app.get('/menu', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/menu.ejs'));
-});
-
-app.get('/home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/home.ejs'));
+    res.render('menu');
 });
 
 app.get('/contactanos', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/contactanos.ejs'));
+    res.render('contactanos');
 });
 
 app.listen(port, () => {
