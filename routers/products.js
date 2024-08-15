20167936386acd272014a584b5815f9252985c83
johannes003 <johannesandres03@gmail.com>
2024-08-15 17:20:46 -0500

    fix

--- routers/products.js
+++ routers/products.js
@@ -19,18 +19,18 @@ const products = [
     { id: 11, name: 'Shawarma de Pollo al Plato', description: 'Pollo, lechuga, tomate y cebolla, mayonesa de ajo y tarator.', price: 15000, image: '/images/shawarma.jpeg' },
     { id: 12, name: 'Shawarma Mixto', description: 'Carne y pollo, lechuga, tomate y cebolla, mayonesa de ajo y tarator.', price: 15000, image: '/images/shawarma_de_carne.jpg' },
 { id: 13, name: 'Shawarma Mixto al Plato', description: 'Carne y pollo, lechuga, tomate y cebolla, mayonesa de ajo y tarator.', price: 15000, image: '/images/shawarma_de_carne_al_plato.jpeg' },
-{ id: 14, name: 'Shish Kebab', description: 'Carne picada puesta en fierros a la parrilla con papas o ensalada.', price: 15000, image: '/images/shish_kebab.jpg' },
+{ id: 14, name: 'Shish Kebab', description: 'Carne picada puesta en fierros a la parrilla con papas o ensalada.', price: 15000, image: '/images/shish_kebab.jpeg' },
 { id: 15, name: 'Shish Taouk', description: 'Brochettes de pollo cortado a cuchillo con papas fritas o ensalada.', price: 15000, image: '/images/shish_kebab_de_carne_picada.jpeg' },
 { id: 16, name: 'Lomo', description: '2 brochettes de lomo cortado a cuchillo con papas fritas o ensalada.', price: 15000, image: '/images/brochette_de_pollo.jpeg' },
-{ id: 17, name: 'Falafel en Sandwich', description: 'Lechuga, cebolla y tomate, salsa de ajo o hummus y salsa de tarator.', price: 15000, image: '/images/falafel.jpg' },
+{ id: 17, name: 'Falafel en Sandwich', description: 'Lechuga, cebolla y tomate, salsa de ajo o hummus y salsa de tarator.', price: 15000, image: '/images/falafel.jpeg' },
 { id: 18, name: 'Carne al Plato', description: 'Carne picada bien condimentada a la parrilla con papas fritas o ensalada.', price: 15000, image: '/images/carne_picada_al_plato_pero_armada_tipo_hamburguesa_compactada_y_cocida.jpeg' },
-{ id: 19, name: 'Pollo Crocante', description: '1/2 porción de supremas de pollo condimentado al estilo crispy.', price: 15000, image: '/images/tenders_de_pollo_con_papas_francesas.jpeg' },
+{ id: 19, name: 'Pollo Crocante', description: '1/2 porción de supremas de pollo condimentado al estilo crispy.', price: 15000, image: '/public/images/' },
 { id: 20, name: 'Toshka', description: 'Pan árabe relleno con carne picada condimentada y cocinado a la parrilla con papas fritas o ensalada.', price: 15000, image: '/images/toshka.jpg' },
 
 // Sección Platos Fríos/Ensaladas
 { id: 21, name: 'Hummus', description: 'Garbanzo, tahine y especias árabes.', price: 10000, image: '/images/Hummus.jpg' },
 { id: 22, name: 'Mtabal (Puré de Berenjenas)', description: 'Berenjenas ahumadas con tahine, yogurt, ajo y sal.', price: 10000, image: '/images/mtabal.jpg' },
-{ id: 23, name: 'Mohammara (Puré de Morrón)', description: 'Morrones ahumados, pan rallado, pimentón en polvo y especias árabes.', price: 10000, image: '/images/mohammara.jpg' },
+{ id: 23, name: 'Mohammara (Puré de Morrón)', description: 'Morrones ahumados, pan rallado, pimentón en polvo y especias árabes.', price: 10000, image: '/images/mohamara.jpg' },
 { id: 24, name: 'Labane (Yogurt)', description: 'Yogurt árabe firme con aceite de oliva y aceitunas.', price: 10000, image: '/images/labane.webp' },
 { id: 25, name: 'Kishke', description: 'Yogurt árabe con trigo burgol, cebolla picada, sal, ajo, perejil y nueces.', price: 10000, image: '/images/kishke.jpg' },
 { id: 26, name: 'Tabule', description: 'Perejil, tomate, cebolla, trigo, aceite de oliva, sal y sal de limón.', price: 10000, image: '/images/tabule.jpg' },
@@ -39,21 +39,21 @@ const products = [
 { id: 29, name: 'Fattoush', description: 'Lechuga, cebolla, tomate, perejil, pepino, aceitunas, queso árabe, pan frito y aderezo de la casa.', price: 10000, image: '/images/ensalada_fattoush_perejil_tomate_cebolla_aceitunas_queso_en_cubitos_pan_arabe_frito_y_nueces.jpeg' },
 
 // Sección Empanadas
-{ id: 30, name: 'Empanada Abierta', description: 'Empanada abierta con carne y especias.', price: 5000, image: '/images/empanada_abierta.jpeg' },
-{ id: 31, name: 'Empanada Cerrada', description: 'Empanada cerrada con carne y especias.', price: 5000, image: '/images/empanada_cerrada.webp' },
+{ id: 30, name: 'Empanada Abierta', description: 'Empanada abierta con carne y especias.', price: 5000, image: '/images/empanada abierta.jpg' },
+{ id: 31, name: 'Empanada Cerrada', description: 'Empanada cerrada con carne y especias.', price: 5000, image: '/images/emapanda cerrada.webp' },
 { id: 32, name: 'Sojok', description: 'Empanada con carne muy condimentada.', price: 5000, image: '/images/sojok.png' },
-{ id: 33, name: 'Empanada de Queso', description: 'Empanada de queso ricotta y perejil.', price: 5000, image: '/images/empanada_queso.jpg' },
-{ id: 34, name: 'Empanada de Espinaca', description: 'Empanada de espinaca con cebolla y especias.', price: 5000, image: '/images/empanada_espinaca.jpg' },
-{ id: 35, name: 'Empanada de Berenjena', description: 'Berenjenas con morrón, cebolla fritos, nueces y especias árabes.', price: 5000, image: '/images/empanada_de_berenjena.jpg' },
+{ id: 33, name: 'Empanada de Queso', description: 'Empanada de queso ricotta y perejil.', price: 5000, image: '/images/empanada queso.jpg' },
+{ id: 34, name: 'Empanada de Espinaca', description: 'Empanada de espinaca con cebolla y especias.', price: 5000, image: '/images/emapanda espianca.jpg' },
+{ id: 35, name: 'Empanada de Berenjena', description: 'Berenjenas con morrón, cebolla fritos, nueces y especias árabes.', price: 5000, image: '/images/empanadas de berenjena.jpg' },
 
 // Sección Bebidas
-{ id: 36, name: 'Coca-Cola Común', description: 'Coca-Cola 500ml.', price: 3000, image: '/images/coca-cola.jpeg' },
-{ id: 37, name: 'Coca-Cola Cero', description: 'Coca-Cola 500ml.', price: 3000, image: '/images/coca-cola_cero.jpeg' },
+{ id: 36, name: 'Coca-Cola Común', description: 'Coca-Cola 500ml.', price: 3000, image: '/images/coca-cola.jpg' },
+{ id: 37, name: 'Coca-Cola Cero', description: 'Coca-Cola 500ml.', price: 3000, image: '/images/coca-cola cero.jpeg' },
 { id: 38, name: 'Sprite Común', description: 'Sprite 500ml.', price: 3000, image: '/images/sprite-comun.jpg' },
 { id: 39, name: 'Sprite Cero', description: 'Sprite cero 500ml.', price: 3000, image: '/images/sprite-cero.webp' },
 { id: 40, name: 'Fanta Común', description: 'Fanta 500ml.', price: 3000, image: '/images/fanta.jpeg' },
-{ id: 41, name: 'Agua con Gas', description: 'Agua con gas 500ml.', price: 3000, image: '/images/agua_con_gas.webp' },
-{ id: 42, name: 'Agua sin Gas', description: 'Agua sin gas 500ml.', price: 3000, image: '/images/agua_sin_gas.webp' },
+{ id: 41, name: 'Agua con Gas', description: 'Agua con gas 500ml.', price: 3000, image: '/images/agua con gas.webp' },
+{ id: 42, name: 'Agua sin Gas', description: 'Agua sin gas 500ml.', price: 3000, image: '/images/agua sin gas.webp' },
 { id: 43, name: 'Levite Manzana', description: 'Levite de manzana 500ml.', price: 3000, image: '/images/levite-manzana.jpg' },
 { id: 44, name: 'Levite Naranja', description: 'Levite de naranja 500ml.', price: 3000, image: '/images/levite-naranja.jpg' },
 { id: 45, name: 'Levite Pera', description: 'Levite de pera 500ml.', price: 3000, image: '/images/levite-pera.webp' },
@@ -61,13 +61,6 @@ const products = [
 { id: 47, name: 'Schweppes Tónica', description: 'Schweppes tónica 500ml.', price: 3000, image: '/images/Schweppes-tonica.webp' },
 { id: 48, name: 'Schweppes Pomelo', description: 'Schweppes pomelo 500ml.', price: 3000, image: '/images/Schweppes-pomelo.webp' },
 { id: 49, name: 'Schweppes Citrus', description: 'Schweppes citrus 500ml.', price: 3000, image: '/images/Schweppes-pomelo.webp' },
-
-// Sección Postres
-{ id: 50, name: 'Mhalabia', description: 'Pudín de leche con sabor a agua de rosas.', price: 5000, image: '/images/mhalabia.jpg' },
-{ id: 51, name: 'Knafeh', description: 'Postre tradicional árabe con queso y pasta de filo.', price: 5000, image: '/images/knafeh.jpg' },
-{ id: 52, name: 'Baklava', description: 'Postre tradicional árabe con nueces y miel.', price: 5000, image: '/images/baklava.jpg' },
-{ id: 53, name: 'Riz bi Laban', description: 'Arroz con leche, canela y esencia de rosas.', price: 5000, image: '/images/riz_bi_laban.jpg' },
-{ id: 54, name: 'Atayef', description: 'Crepas rellenas de nueces y miel.', price: 5000, image: '/images/atayef.jpg' }
 ];
 
 // Función para obtener un producto por su ID
