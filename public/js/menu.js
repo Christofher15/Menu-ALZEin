c5faca83cfc12e4c59a7d69cc9fc69c0b022de23
johannes003 <johannesandres03@gmail.com>
2024-08-27 10:47:40 -0500

    fix code

--- public/js/menu.js
+++ public/js/menu.js
@@ -2,23 +2,20 @@ document.querySelector('.menu-toggle').addEventListener('click', function() {
     const nav = document.querySelector('.main-nav');
     const menuIcon = document.querySelector('.menu-icon');
     
-    // Alterna la clase 'active' para mostrar/ocultar el menú
     nav.classList.toggle('active');
     menuIcon.classList.toggle('active');
 
-    // Animación de deslizamiento
     if (nav.classList.contains('active')) {
-        nav.style.maxHeight = nav.scrollHeight + "px"; // Expande el menú
+        nav.style.maxHeight = nav.scrollHeight + "px"; 
     } else {
-        nav.style.maxHeight = "0"; // Colapsa el menú
+        nav.style.maxHeight = "0"; 
     }
 });
 
-// Asegúrate de restablecer la altura máxima al cambiar el tamaño de la ventana
 window.addEventListener('resize', function() {
     const nav = document.querySelector('.main-nav');
     if (window.innerWidth > 768) {
-        nav.style.maxHeight = null; // Elimina el estilo en pantallas grandes
-        nav.classList.remove('active'); // Elimina la clase active si la ventana se expande
+        nav.style.maxHeight = null; 
+        nav.classList.remove('active'); 
     }
 });
